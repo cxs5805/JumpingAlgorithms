76a371b0a84e98ac6b55f2b2399dc0b8339573a3
cxs5805 <cxs5805@g.rit.edu>
2017-08-29 15:58:00 -0400

    Removed walking via forces (for now)

---
+++ Movement.cs
@@ -0,0 +1,418 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // kinematic attributes
+    public Vector3 vel;
+    public Vector3 acc;
+    public float walkSpeed;
+
+    // dynamic attributes
+    public float mass;
+    public float maxVel;
+    public float minVel;
+    public float maxAcc;
+
+    // fun jumping attributes
+    public float gravity;
+    public float height;
+    private float initJumpV; // this = sqrt (2 * gravity * height)
+
+    // flag attributes
+    // jumping
+    public bool up;
+
+    // fast-falling
+    private bool fastfall;
+    public bool doFastfall; // press Alpha1 to toggle whether fastfall acts
+    
+    // Simon Belmont style jump
+    private bool belmont;
+    public bool doBelmont; // press Alpha2 to toggle whether belmont acts
+    private KeyCode dir; // track current key for direction of belmont jump
+
+    // variable height jumping
+    public bool doVar; // press Alpha3 to toggle whether VHJ applies
+    private int counter = 0; // keeps track of # of frames button held
+    public int maxCounts; // maximum number of count increments before peak
+
+    // double jumping
+    public bool doDoubleJump; // press Alpha4 to toggle double jumping
+    private bool doubleJump; // if true, press K if up==true to jump again
+    private int jumpCounter = 0; // current number of times player has jumped
+    public int maxJumps; // maximum number of jumps allowed
+
+    // walking with forces attributes
+    public float friction;
+    public bool doFriction;
+
+    // floor object attribute
+    public GameObject floorObj;
+    private float floor;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // assume actor spawns in the air
+        // so, it's up and fastfalling if doFastfall
+        up = true;
+        if (doFastfall)
+            fastfall = true;
+
+        // also, assume belmont and fall straight if doBelmont
+        dir = KeyCode.None;
+        if (doBelmont)
+            belmont = true;
+
+        // also, assume able to double jump
+        if (doDoubleJump)
+            doubleJump = true;
+
+        // calculate initial jump delta v
+        initJumpV = Mathf.Sqrt(2 * gravity * height);
+        Debug.Log("initial jumping force magnitude = " + initJumpV);
+
+        // set floor value for jumping
+        floor = floorObj.transform.position.y + 1.7f;
+        Debug.Log("floor height = " + floor);
+
+        // calculate minimum velocity based on friction
+        minVel = 0.01f;//friction * 0.1f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //up = true;
+        //if (transform.position.y <= floor && up)
+        //    Land(floor);
+        ProcessInput(); // process input for left/right and jumping
+
+        if (up)
+        {
+            if (fastfall)
+            {
+                ApplyForce(new Vector3(0f, -4.5f * gravity)); // apply increased gravity after the peak of a jump
+            }
+            else
+            {
+                ApplyForce(new Vector3(0f, -gravity)); // apply force due to gravity
+            }
+        }
+        UpdatePos(); // update current position based on previous state
+        //ProcessInput(); // process input for left/right and jumping
+    }
+
+    void UpdatePos()
+    {
+        /*
+        // cap acceleration if needed
+        if (acc.magnitude > maxAcc)
+        {
+            acc.Normalize();
+            acc *= maxAcc;
+        }
+        */
+
+        // EULER
+        // add velocity to acceleration * delta(t)
+        vel += acc * Time.deltaTime;
+
+
+        // cap velocity if needed
+        if (Mathf.Abs(vel.x) > maxVel)
+        {
+            if (vel.x < 0.0f)
+                vel.x = -maxVel;
+            else
+                vel.x = maxVel;
+        }
+
+        // make sure our guy actually stops!
+        if (Mathf.Abs(vel.x) < Mathf.Abs(minVel))
+            vel.x = 0.0f;
+
+        // add position to velocity * delta(t)
+        transform.position += vel * Time.deltaTime;
+
+        //if (transform.position.y <= floor && up)
+        //    Land(floor);
+        //else
+        //{
+            // white if airborn
+            //up = true;
+            //doubleJump = true;
+            //GetComponentInChildren<SpriteRenderer>().color = Color.white;
+        //}
+        /*
+        // fix position to the floor
+        if (transform.position.y <= floor && up)
+        {
+            transform.position = new Vector3(transform.position.x, floor);
+            acc.y = 0.0f; // not moving, so acceleration is zero
+            vel.y = 0.0f; // and velocity to zero
+            up = false; // grounded...
+            belmont = false; // ...so don't work belmont magic
+            doubleJump = false; // disable double jumping too
+            // this is a problem line
+            // because this if statement is checked every frame
+            // even when already grounded
+            // but can't we just add an extra conditional??????
+
+            // reset counter
+            counter = 0;
+            jumpCounter = 0;
+            
+            // new debugging shenanigans, black if grounded
+            //GetComponentInChildren<SpriteRenderer>().color = Color.black;
+        }
+        else
+        {
+            // white if airborn
+            //up = true;
+            //doubleJump = true;
+            //GetComponentInChildren<SpriteRenderer>().color = Color.white;
+        }
+        */
+
+        // test for fastfalling
+        if (up && vel.y <= 0f && doFastfall)
+        {
+            fastfall = true;
+            //GetComponentInChildren<SpriteRenderer>().color = Color.red;
+        }
+        else
+        {
+            fastfall = false;
+            //GetComponentInChildren<SpriteRenderer>().color = Color.blue;
+        }
+
+        // debug for DOUBLE JUMP flag
+        // green if
+        //if (doubleJump)
+        //    GetComponentInChildren<SpriteRenderer>().color = Color.green;
+        // red if not
+        //else
+        //    GetComponentInChildren<SpriteRenderer>().color = Color.red;
+    }
+
+    public void Land(float h)
+    {
+        // fix position to the floor
+        transform.position = new Vector3(transform.position.x, h);
+
+        // 7/21/17
+        // set ALL velocities to zero
+        //acc = Vector3.zero;
+        //vel = Vector3.zero;
+
+        acc.y = 0.0f; // not moving, so acceleration is zero
+        vel.y = 0.0f; // and velocity to zero
+        up = false; // grounded...
+        belmont = false; // ...so don't work belmont magic
+        doubleJump = false; // disable double jumping too
+        // this is a problem line
+        // because this if statement is checked every frame
+        // even when already grounded
+        // but can't we just add an extra conditional??????
+
+        // reset counter
+        counter = 0;
+        jumpCounter = 0;
+
+        // new debugging shenanigans, black if grounded
+        //GetComponentInChildren<SpriteRenderer>().color = Color.black;
+    }
+
+    void ProcessInput()
+    {
+        // toggle fastfall, belmont, and var
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            doFastfall = !doFastfall;
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            doBelmont = !doBelmont;
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            doVar = !doVar;
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            doDoubleJump = !doDoubleJump;
+
+        if (!belmont)
+            Walk(); // walk by applying a delta(v) in x, independent of y
+        if (doFriction)
+            ApplyFriction();
+        Jump(); // jumping (this is the fun part)
+    }
+
+    void Toggle(KeyCode key, bool flag)
+    {
+        if (Input.GetKeyDown(key))
+            flag = !flag;
+    }
+
+    void ApplyForce(Vector3 force)
+    {
+        // F = MA
+        // A = M/F
+        acc += force / mass;
+    }
+
+    void Walk()
+    {
+        dir = KeyCode.None;
+        vel.x = 0;
+
+        // walk left
+        if (Input.GetKey(KeyCode.A))
+        {
+            // regular walking
+            //transform.position = new Vector3(transform.position.x - walkSpeed, transform.position.y);
+            vel.x = -walkSpeed;
+            //vel.x = -walkSpeed;
+            dir = KeyCode.A;
+        }
+        // walk right
+        if (Input.GetKey(KeyCode.D))
+        {
+            //transform.position = new Vector3(transform.position.x + walkSpeed, transform.position.y);
+            vel.x = walkSpeed;
+            //vel.x = walkSpeed;
+            dir = KeyCode.D;
+        }
+    }
+
+    void Jump()
+    {
+        // start single jump
+        if (Input.GetKeyDown(KeyCode.K) && !up)
+        {
+            // OLD
+            // apply jumping force
+            //ApplyForce(new Vector3(0f, initJumpV));
+
+            // NEW
+            // just change velocity directly and discontinuously ONCE
+            //vel = new Vector3(vel.x, vel.y + initJumpV);
+            vel.y += initJumpV;
+            up = true;
+
+            // get counter started
+            ++counter;
+
+            // start belmont
+            if (doBelmont)
+                belmont = true;
+
+            // BAD, MOVE TO KEYUP
+            /*// also start double jumping
+            if (doDoubleJump)
+            {
+                doubleJump = true;
+                
+                // DEBUG
+                Debug.Log("Now you can double jump!");
+            }*/
+
+            // increment jump counter once - it's the first jump!
+            jumpCounter++;
+        }
+
+        // variable height jumping (only if flag = true)
+        if (doVar)
+        {
+            // press and hold for longer jump for maxCounts number of frames
+            if (Input.GetKey(KeyCode.K) && counter > 0 && counter < maxCounts)
+            {
+                ++counter;
+                vel.y += initJumpV;// = new Vector3(vel.x, vel.y + initJumpV);
+            }
+            else
+            {
+                counter = 0;
+                //belmont = false;
+            }
+        }
+        
+        // belmont magic - fix x velocity
+        if (belmont)
+        {
+            if (dir == KeyCode.A)
+            {
+                //transform.position = new Vector3(transform.position.x - walkSpeed, transform.position.y);
+                vel.x = -walkSpeed;
+            }
+            else if (dir == KeyCode.D)
+            {
+                //transform.position = new Vector3(transform.position.x + walkSpeed, transform.position.y);
+                vel.x = walkSpeed;
+            }
+        }
+
+        // NEW KEYUP IF STATEMENT
+        if (up && Input.GetKeyUp(KeyCode.K) && doDoubleJump && !doubleJump)
+        {
+            doubleJump = true;
+
+            // DEBUG
+            //Debug.Log("Now you can double jump!");
+        }
+        /* HERE'S WHERE PROBLEMS ARE/WERE
+         * this if statement is true on the first frame, we don't want that!
+         * -
+         * DESIRED BEHAVIOR
+         * user presses K once, only one jump
+         * regardless of whether held down or let go
+         * after letting up, enable double jumping
+         * NOW, if user pressees jump again, do the second jump
+         * -
+         * IMPLEMENTATION
+         * keydown k => jump (first if statement)
+         * don't change doubleJump here, even if canDoubleJump!!!!!
+         * new if statemnt
+         * keyup k and canDoubleJump => doubleJump = true
+         * else the following if statement
+        */
+        // double jump
+        else if (Input.GetKeyDown(KeyCode.K) && doubleJump && jumpCounter < maxJumps)
+        {
+            // set vel and acc to zero, it's as if jumping from ground
+            vel.y = 0f;
+            acc.y = 0f;
+
+            // now get double jump going
+            vel.y += initJumpV;
+
+            // set double jump flag to false
+            doubleJump = false;
+
+            // reset counter for new jump
+            counter = 1;
+            jumpCounter++;
+        }
+
+        //Debug.Log("counter = " + jumpCounter);
+    }
+
+    void ApplyFriction()
+    {
+        // make sure all of the following are true
+        // no directional buttons pressed
+        // velocity is nonzero
+        // AND grounded
+        if(dir == KeyCode.None && vel.x != 0.0f && !up)
+        {
+            // then set temp equal to friction constant
+            // and if velocity is positive, make temp = -temp
+            // then apply force (directly or via apply force? => try both!)
+            float fricTemp = friction;
+            if (vel.x > 0.0f)
+                fricTemp = -fricTemp;
+
+            vel.x += fricTemp;
+            
+            // DEBUG
+            //Debug.Log("fricTemp = " + fricTemp);
+        }
+    }
+}
---
+++ Platforming.cs
@@ -0,0 +1,277 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platforming : MonoBehaviour
+{
+    // our guy's hitbox
+    private Bounds hitbox;
+    
+    // array (or list?) to hold all platforms
+    private GameObject[] platforms;
+    private Bounds[] boundaries;
+
+    // we need to keep track of the current platform
+    // in order for the player to fall off of it
+    // this makes sense because the player can only
+    // be standing on EXACTLY ONE platform
+    private Bounds currPlatform;
+
+    private Movement movement;
+
+    // 8/27/17
+    // in order for side collisions to work properly
+    // we may need to keep track of the position on
+    // the previous frame
+    private Vector3 prevPos;
+
+    //private int landTimer;
+    //public int landDuration;
+
+	// Use this for initialization
+	void Start ()
+    {
+        prevPos = transform.position;
+        
+        // assume midair, so not landed yet
+        //landTimer = 0;
+
+        // get movement script and hitbox
+        movement = gameObject.GetComponent<Movement>();
+        hitbox = gameObject.GetComponent<BoxCollider2D>().bounds;
+
+		// populate platforms array
+        platforms = GameObject.FindGameObjectsWithTag("Platform");
+        boundaries = new Bounds[platforms.Length];
+
+        for (int i = 0; i < platforms.Length; ++i)
+        {
+            // get all the bounds structs from each platform's AABB collider
+            boundaries[i] = platforms[i].GetComponent<Collider2D>().bounds;
+
+            // DEBUG
+            Debug.Log("Index " + i + " is " + platforms[i].name +
+                "\r\nMin X = " + boundaries[i].min.x +
+                "\r\nMax X = " + boundaries[i].max.x +
+                "\r\nMin Y = " + boundaries[i].min.y +
+                "\r\nMax Y = " + boundaries[i].max.y);
+        }
+        // DEBUG
+        Debug.Log("Player" +
+            "\r\nMin X = " + hitbox.min.x +
+            "\r\nMax X = " + hitbox.max.x +
+            "\r\nMin Y = " + hitbox.min.y +
+            "\r\nMax Y = " + hitbox.max.y);
+
+        Debug.Log("Player pos " +
+            "\r\nX = " + transform.position.x +
+            "\r\nY = " + transform.position.y);
+
+        // now that platforms array has been populated, just
+        // initialize the current platform as null
+
+        // now that currPlatform is a Bounds, this line 
+        // generates compiler errors, so I just commented
+        // it out
+        //currPlatform = null;
+        currPlatform = new Bounds(new Vector3(float.MinValue, float.MinValue), new Vector3(float.MinValue, float.MinValue));
+        //Debug.Log("currPlatform = " + currPlatform);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // test time simulation
+        Debug.Log("time => " + Time.timeScale);
+
+        if (movement.up)
+            CheckAllPlatforms();
+        else
+            CheckCurrPlatform();
+
+        // get hitbox on every frame
+        hitbox = gameObject.GetComponent<BoxCollider2D>().bounds;
+
+        // DEBUG
+        //Debug.Log("Player" +
+        //    "\r\nMin X = " + hitbox.min.x +
+        //    "\r\nMax X = " + hitbox.max.x +
+        //    "\r\nMin Y = " + hitbox.min.y +
+        //    "\r\nMax Y = " + hitbox.max.y);
+	}
+
+    void CheckAllPlatforms()
+    {
+        foreach (Bounds b in boundaries)
+        {
+        //    // 8/5/17
+        //    // new way, hopefully the good way
+        //    // check sides, and only do other 2
+        //    // checks if not hitting sides
+        //    if (HittingSides(b))
+        //    {
+        //        Debug.Log("hitting sides");
+        //    }
+        //    else
+        //    {
+        //        CheckTop(b);
+        //        CheckBottom(b);
+        //    }
+
+            // old way - do all 3 checks every time
+            CheckTop(b);
+            CheckSides(b);
+            CheckBottom(b);
+        }
+    }
+
+    void CheckTop(Bounds b)
+    {
+        if
+            (
+                // old version with position
+                transform.position.x >= b.min.x &&
+                transform.position.x <= b.max.x &&
+                transform.position.y <= b.max.y &&
+                transform.position.y >= b.min.y //&&
+                //movement.vel.y < 0
+
+                // new version with hitbox
+                //hitbox.min.x <= b.max.x &&
+                //hitbox.max.x >= b.min.x &&
+                //transform.position.y /*hitbox.min.y*/ <= b.max.y
+                //&& transform.position.y >= b.min.y
+                //&&hitbox.max.y >= b.min.y
+            )
+        {
+            // top edge - landing!
+            if (movement.vel.y <= 0)
+            {
+                movement.Land(b.max.y);
+                // DEBUG
+                //Debug.Log("Hey, land! and current platform = " + b.ToString());
+                // 7/11/17 - NOW, SET CURRENT PLATFORM HERE
+                currPlatform = b;
+            }
+            /*
+                // bottom edge
+            else if (movement.vel.y >= 0)
+            {
+                Debug.Log("bottom edge");
+            }
+            //*/
+        }
+        /*
+            // let's try writing the logic for having the guy fall off
+        // the conditions for it to fall off are that
+        // A. it's already grounded
+        // B. it walks off the left or right edge
+        // 7/11/17 - MOVE THIS OUT OF THE FOREACH LOOP
+        // BUT STILL CHECK EVERY FRAME
+        else if (!movement.up &&
+            (transform.position.x < b.min.x ||
+                transform.position.x > b.max.x))
+        {
+            //Debug.Log("You should fall off now");
+            //break;
+            // now that the conditions are correct for falling off,
+            // set up = false!
+            //movement.up = false;
+        }
+        //*/
+    }
+
+    void CheckSides(Bounds b)
+    {
+        //// left and right sides
+        //if 
+        //(
+        //    (
+        //        hitbox.max.x >= b.min.x ||
+        //        hitbox.min.x <= b.max.x
+        //    ) &&
+        //    hitbox.min.y <= b.max.y &&
+        //    hitbox.max.y >= b.min.y &&
+        //    movement.vel.x != 0 &&
+        //    !movement.up
+        //)
+        //{
+        //    movement.vel.x = 0;
+        //    Debug.Log("You're hitting the sides");
+        //}
+
+
+        // 8/27/17
+        // if, on previous frame, player was not inside,
+        // and it IS inside on current frame,...
+        // then, pop player out from side
+        if
+        (
+                (
+                    prevPos.x <= b.min.x ||
+                    prevPos.x >= b.max.x
+                )
+        )
+        {
+        }
+    }
+
+    bool HittingSides(Bounds b)
+    {
+        bool hitting = false;
+        // left and right sides
+        if 
+        (
+            (
+                hitbox.max.x >= b.min.x ||
+                hitbox.min.x <= b.max.x
+            ) &&
+            hitbox.min.y <= b.max.y &&
+            hitbox.max.y >= b.min.y &&
+            movement.vel.x != 0
+            &&movement.up
+        )
+        {
+            movement.vel.x = 0;
+            hitting = true;
+            Debug.Log("You're hitting the sides");
+        }
+        return hitting;
+    }
+
+    void CheckBottom(Bounds b)
+    {
+        if
+        (
+            hitbox.max.x >= b.min.x &&
+            hitbox.min.x <= b.max.x &&
+            hitbox.max.y <= b.max.y &&
+            hitbox.max.y >= b.min.y
+            &&movement.vel.y >= 0
+        )
+        {
+            movement.vel.y = 0;//-movement.vel.y;
+            //Debug.Log("you're hitting the bottom");
+        }
+    }
+
+    void CheckCurrPlatform()
+    {
+        if
+            (
+                currPlatform.center.x != float.MinValue &&
+                !movement.up &&
+                (
+                    transform.position.x < currPlatform.min.x ||
+                    transform.position.x > currPlatform.max.x
+                )
+            )
+        {
+            //Debug.Log("You should fall off now");
+            //break;
+            // now that the conditions are correct for falling off,
+            // set up = false!
+            movement.up = true;
+        }
+    }
+}
